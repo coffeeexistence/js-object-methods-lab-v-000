e0870602d7778f6e728861523234da1876ddd7ff
John Anthony Rivera <askinganthony@gmail.com>
2016-02-22 15:52:40 -0800

    Done.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // code solution here
+function President(name, politicalParty, yearsInOffice, homeState){
+	this.name=name;
+	this.politicalParty=politicalParty;
+	this.yearsInOffice=yearsInOffice;
+	this.homeState=homeState;
+
+	this.veto = function(){
+    	return "NO!" 
+  	}
+
+  	this.passBill = function(){
+    	return "You can do that!"
+  	}
+
+  	this.doCharity = function(){
+    	return "I like the help people."
+  	}
+
+  	this.conductPressInterview = function(){
+    	return "I am proud to be an American."
+  	}
+
+  	this.sayHi = function(){
+    	return "Hi, my name is "+this.name+". I am from "+homeState+". I represent the "+politicalParty+"s, and was in office "+yearsInOffice+"."
+  	}
+}
+
